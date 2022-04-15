8ae4db57e246eb0eebb3017043a58e1676178cf5
AlexOliinyk <alex.oliinyk@gmail.com>
2022-04-15 12:47:04 +0300

    Initial Commit

---
+++ Git Searcher/Git_SearcherApp.swift
@@ -0,0 +1,17 @@
+//
+//  Git_SearcherApp.swift
+//  Git Searcher
+//
+//  Created by Oleksandr Oliinyk on 15.04.2022.
+//
+
+import SwiftUI
+
+@main
+struct Git_SearcherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
